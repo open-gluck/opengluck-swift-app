8759381cb2bd55179aa75ae6b43a721ca203230f
Christopher Allène <cwiiss@gmail.com>
2025-09-14 14:36:22 +0200

    fix: enable task refresh on mac

--- OpenGluck Phone App/PhoneAppDelegate.swift
+++ OpenGluck Phone App/PhoneAppDelegate.swift
@@ -113,20 +113,18 @@ class PhoneAppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate, O
             self.handleAppRefresh(task: task as! BGAppRefreshTask)
         }
 
-        if !ProcessInfo.processInfo.isiOSAppOnMac {
-            NotificationCenter.default.addObserver(forName:UIApplication.didBecomeActiveNotification, object: nil, queue: nil) { (_) in
-                Task {
-                    await MainActor.run {
-                        OpenGluckEnvironment.enableAutoUpdate = true
-                    }
+        NotificationCenter.default.addObserver(forName:UIApplication.didBecomeActiveNotification, object: nil, queue: nil) { (_) in
+            Task {
+                await MainActor.run {
+                    OpenGluckEnvironment.enableAutoUpdate = true
                 }
             }
-            NotificationCenter.default.addObserver(forName:UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { (_) in
-                Task {
-                    await MainActor.run {
-                        OpenGluckEnvironment.enableAutoUpdate = false
-                        self.scheduleBackgroundTask()
-                    }
+        }
+        NotificationCenter.default.addObserver(forName:UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { (_) in
+            Task {
+                await MainActor.run {
+                    OpenGluckEnvironment.enableAutoUpdate = false
+                    self.scheduleBackgroundTask()
                 }
             }
         }
