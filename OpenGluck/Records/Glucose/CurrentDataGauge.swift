17a1cbd0604f39f774f6b6cf780ba097c05e10d0
Christopher Allène <cwiiss@gmail.com>
2025-09-15 11:13:52 +0200

    fix: image not showing on watchOS

--- OpenGluck/Records/Glucose/CurrentDataGauge.swift
+++ OpenGluck/Records/Glucose/CurrentDataGauge.swift
@@ -72,13 +72,18 @@ struct CurrentDataGauge: View {
                 }
                 if let string {
                     if string == "?" {
-                        // when text is ? display instead a grayish version of our icon,
-                        // that better conveys we don't have up-to-date data
-                        Image("SmallUnknownAppIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(Circle())
-                            .scaleEffect(2)
+                        if true {
+                            // For some reasons this image is not showing on Apple Watch
+                            Text("?")
+                        } else {
+                            // when text is ? display instead a grayish version of our icon,
+                            // that better conveys we don't have up-to-date data
+                            Image("SmallUnknownAppIcon")
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(Circle())
+                                .scaleEffect(2)
+                        }
                     } else {
                         Text(string)
                             .foregroundStyle(colorText)
