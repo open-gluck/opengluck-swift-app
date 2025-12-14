b54d8886ac5ac321b5b520535a8c1d91443cb32d
Christopher Allène <cwiiss@gmail.com>
2025-12-14 07:52:55 +0100

    feat: add macOS app

---
+++ OpenGluck Mac App/MacContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OG Mac
+//
+//  Created by Christopher Allène on 13/6/25.
+//
+
+import SwiftUI
+
+struct MacContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MacContentView()
+}
