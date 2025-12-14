ae2a4e47fc6b6795820bcd354940c6b5afc32cab
Christopher Allène <66381046+callms@users.noreply.github.com>
2025-12-14 07:53:31 +0100

    feat: add macOS app (#47)
    Co-authored-by: Christopher Allène <cwiiss@gmail.com>

---
+++ OpenGluck Mac App/OpenGluckMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  OG_MacApp.swift
+//  OG Mac
+//
+//  Created by Christopher Allène on 13/6/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenGluckMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MacContentView()
+        }
+    }
+}
