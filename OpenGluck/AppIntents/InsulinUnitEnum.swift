386aff46e4f75e853f56a4e429642796c03f66c8
Christopher Allène <cwiiss@gmail.com>
2026-03-28 18:04:52 +0100

    fix: insulin intents

--- OpenGluck/AppIntents/InsulinUnitEnum.swift
+++ OpenGluck/AppIntents/InsulinUnitEnum.swift
@@ -1,142 +1,48 @@
 import AppIntents
 
-/* 
- For some reasons Siri won't parse integers in intents but is glad to parse an enum, so here we go.
- FIXME: seems this is broken in iOS 17.4 and watchOS 10.4 :/
- */
+enum InsulinUnitEnum: Int, Codable, Sendable, CaseIterable {
+    case one = 1, two, three, four, five
+    case six, seven, eight, nine, ten
+    case eleven, twelve, thirteen, fourteen, fifteen
+    case sixteen, seventeen, eighteen, nineteen, twenty
+    case twentyOne, twentyTwo, twentyThree, twentyFour, twentyFive
+    case twentySix, twentySeven, twentyEight, twentyNine
 
-#if true
-enum InsulinUnitEnum: String, Codable, Sendable {
-    case value1 = "1"
-    case value2 = "2"
-    case value3 = "3"
-    case value4 = "4"
-    case value5 = "5"
-    case value6 = "6"
-    case value7 = "7"
-    case value8 = "8"
-    case value9 = "9"
-    case value10 = "10"
-    case value11 = "11"
-    case value12 = "12"
-    case value13 = "13"
-    case value14 = "14"
-    case value15 = "15"
-    case value16 = "16"
-    case value17 = "17"
-    case value18 = "18"
-    case value19 = "19"
-    case value20 = "20"
-    case value21 = "21"
-    case value22 = "22"
-    case value23 = "23"
-    case value24 = "24"
-    case value25 = "25"
-    case value26 = "26"
-    case value27 = "27"
-    case value28 = "28"
-    case value29 = "29"
+    var units: Int { rawValue }
 }
 
 extension InsulinUnitEnum: AppEnum {
-    static let typeDisplayRepresentation: TypeDisplayRepresentation = TypeDisplayRepresentation(stringLiteral: "Insulin Unit")
-    
-    static let caseDisplayRepresentations: [InsulinUnitEnum : DisplayRepresentation] = [
-        .value1 : DisplayRepresentation(stringLiteral: "1"),
-        .value2 : DisplayRepresentation(stringLiteral: "2"),
-        .value3 : DisplayRepresentation(stringLiteral: "3"),
-        .value4 : DisplayRepresentation(stringLiteral: "4"),
-        .value5 : DisplayRepresentation(stringLiteral: "5"),
-        .value6 : DisplayRepresentation(stringLiteral: "6"),
-        .value7 : DisplayRepresentation(stringLiteral: "7"),
-        .value8 : DisplayRepresentation(stringLiteral: "8"),
-        .value9 : DisplayRepresentation(stringLiteral: "9"),
-        .value10 : DisplayRepresentation(stringLiteral: "10"),
-        .value11 : DisplayRepresentation(stringLiteral: "11"),
-        .value12 : DisplayRepresentation(stringLiteral: "12"),
-        .value13 : DisplayRepresentation(stringLiteral: "13"),
-        .value14 : DisplayRepresentation(stringLiteral: "14"),
-        .value15 : DisplayRepresentation(stringLiteral: "15"),
-        .value16 : DisplayRepresentation(stringLiteral: "16"),
-        .value17 : DisplayRepresentation(stringLiteral: "17"),
-        .value18 : DisplayRepresentation(stringLiteral: "18"),
-        .value19 : DisplayRepresentation(stringLiteral: "19"),
-        .value20 : DisplayRepresentation(stringLiteral: "20"),
-        .value21 : DisplayRepresentation(stringLiteral: "21"),
-        .value22 : DisplayRepresentation(stringLiteral: "22"),
-        .value23 : DisplayRepresentation(stringLiteral: "23"),
-        .value24 : DisplayRepresentation(stringLiteral: "24"),
-        .value25 : DisplayRepresentation(stringLiteral: "25"),
-        .value26 : DisplayRepresentation(stringLiteral: "26"),
-        .value27 : DisplayRepresentation(stringLiteral: "27"),
-        .value28 : DisplayRepresentation(stringLiteral: "28"),
-        .value29 : DisplayRepresentation(stringLiteral: "29"),
-    ]
-    
-    static let allCases: [InsulinUnitEnum] = [
-        .value1,
-        .value2,
-        .value3,
-        .value4,
-        .value5,
-        .value6,
-        .value7,
-        .value8,
-        .value9,
-        .value10,
-        .value11,
-        .value12,
-        .value13,
-        .value14,
-        .value15,
-        .value16,
-        .value17,
-        .value18,
-        .value19,
-        .value20,
-        .value21,
-        .value22,
-        .value23,
-        .value24,
-        .value25,
-        .value26,
-        .value27,
-        .value28,
-        .value29
+    static let typeDisplayRepresentation: TypeDisplayRepresentation = "Insulin Unit"
+
+    static let caseDisplayRepresentations: [InsulinUnitEnum: DisplayRepresentation] = [
+        .one: DisplayRepresentation(title: "one", synonyms: ["1"]),
+        .two: DisplayRepresentation(title: "two", synonyms: ["2"]),
+        .three: DisplayRepresentation(title: "three", synonyms: ["3"]),
+        .four: DisplayRepresentation(title: "four", synonyms: ["4"]),
+        .five: DisplayRepresentation(title: "five", synonyms: ["5"]),
+        .six: DisplayRepresentation(title: "six", synonyms: ["6"]),
+        .seven: DisplayRepresentation(title: "seven", synonyms: ["7"]),
+        .eight: DisplayRepresentation(title: "eight", synonyms: ["8"]),
+        .nine: DisplayRepresentation(title: "nine", synonyms: ["9"]),
+        .ten: DisplayRepresentation(title: "ten", synonyms: ["10"]),
+        .eleven: DisplayRepresentation(title: "eleven", synonyms: ["11"]),
+        .twelve: DisplayRepresentation(title: "twelve", synonyms: ["12"]),
+        .thirteen: DisplayRepresentation(title: "thirteen", synonyms: ["13"]),
+        .fourteen: DisplayRepresentation(title: "fourteen", synonyms: ["14"]),
+        .fifteen: DisplayRepresentation(title: "fifteen", synonyms: ["15"]),
+        .sixteen: DisplayRepresentation(title: "sixteen", synonyms: ["16"]),
+        .seventeen: DisplayRepresentation(title: "seventeen", synonyms: ["17"]),
+        .eighteen: DisplayRepresentation(title: "eighteen", synonyms: ["18"]),
+        .nineteen: DisplayRepresentation(title: "nineteen", synonyms: ["19"]),
+        .twenty: DisplayRepresentation(title: "twenty", synonyms: ["20"]),
+        .twentyOne: DisplayRepresentation(title: "twenty-one", synonyms: ["21"]),
+        .twentyTwo: DisplayRepresentation(title: "twenty-two", synonyms: ["22"]),
+        .twentyThree: DisplayRepresentation(title: "twenty-three", synonyms: ["23"]),
+        .twentyFour: DisplayRepresentation(title: "twenty-four", synonyms: ["24"]),
+        .twentyFive: DisplayRepresentation(title: "twenty-five", synonyms: ["25"]),
+        .twentySix: DisplayRepresentation(title: "twenty-six", synonyms: ["26"]),
+        .twentySeven: DisplayRepresentation(title: "twenty-seven", synonyms: ["27"]),
+        .twentyEight: DisplayRepresentation(title: "twenty-eight", synonyms: ["28"]),
+        .twentyNine: DisplayRepresentation(title: "twenty-nine", synonyms: ["29"]),
     ]
 }
-
-#else
-/// the same hack, with AppEntity
-/// doesn't work on watchOS 10.4
-
-struct InsulinAppEntity: Identifiable {
-    var id: String { "\(units)" }
-    let units: Int
-}
-
-extension InsulinAppEntity: AppEntity {
-    static let defaultQuery = InsulinAppEntityQuery()
-    static let typeDisplayRepresentation: TypeDisplayRepresentation = "Insulin units"
-    var displayRepresentation: DisplayRepresentation {
-        DisplayRepresentation(title: "\(units)")
-    }
-}
-
-struct InsulinAppEntityQuery: EntityQuery {
-    func entities(for identifiers: [String]) async throws -> [InsulinAppEntity] {
-        /// Filter all available groups using the given identifiers
-        try await suggestedEntities().filter { identifiers.contains($0.id) }
-    }
-    
-    func suggestedEntities() async throws -> [InsulinAppEntity] {
-        /// Return a list of suggested favorite groups to use
-        (1...25).map { InsulinAppEntity(units: $0) }
-    }
-    
-    func defaultResult() async -> InsulinAppEntity? {
-        /// Return default selected group
-        nil
-    }
-}
-#endif
